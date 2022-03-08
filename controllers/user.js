8951573625d58f89d49b97af55906c02b72abb98
Avellow <avellow@yandex.ru>
2022-03-08 11:05:32 +0300

    feature добавить создание пользователя

--- controllers/user.js
+++ controllers/user.js
@@ -13,3 +13,12 @@ module.exports.getUser = (req, res) => {
     .then(user => res.send({ data: user }))
     .catch(err => res.status(500).send({ message: err.message }));
 }
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User
+    .create({ name, about, avatar })
+    .then(user => res.send({ data: user }))
+    .catch(err => res.status(500).send({ message: err.message }));
+}
