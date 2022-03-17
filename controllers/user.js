3b5c0d85fdbc6f0b762009853b769a1d42b66479
Avellow <avellow@yandex.ru>
2022-03-17 12:24:06 +0300

    feature создавать новые поля при создании пользователя, хэш пароля

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const { NotFoundError, checkError } = require('../errors/errors');
 
@@ -19,11 +20,25 @@ module.exports.getUser = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const {
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  } = req.body;
 
-  User
-    .create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User
+      .create({
+        name,
+        about,
+        avatar,
+        email,
+        password: hash,
+      })
+      .then((user) => res.send({ data: user })))
     .catch((err) => checkError(err, res));
 };
 
