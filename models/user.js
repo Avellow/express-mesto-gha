1137f81816bd327176b827c991785c424953296f
Avellow <avellow@yandex.ru>
2022-03-17 11:42:43 +0300

    feature добавлены поля email&pass к схеме user, пакет validator

--- models/user.js
+++ models/user.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+  },
   name: {
     type: String,
     minlength: 2,
