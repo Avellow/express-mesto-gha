3aaba0c6b72106a24c8b7cbe6fcc883d67e266e6
Avellow <avellow@yandex.ru>
2022-03-18 11:53:01 +0300

    feature создать мидлвэр авторизации с токеном

---
+++ middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const handleAuthError = (res) => {
+  res
+    .status(401)
+    .send({ message: 'Необходима авторизация' });
+};
+
+const extractBearerHeader = (header) => header.replace('Bearer', '');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.body;
+
+  if (!authorization || !authorization.startsWith('Bearer')) {
+    return handleAuthError(res);
+  }
+
+  const token = extractBearerHeader(authorization);
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'some-secret');
+  } catch (err) {
+    return handleAuthError(res);
+  }
+
+  req.user = payload;
+
+  next();
+};
