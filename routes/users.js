cdb3fcff4c5573fc97292f94471669df3e73b1b9
Avellow <avellow@yandex.ru>
2022-03-19 10:33:39 +0300

    feature добавить новый роут и обработчик

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,11 @@ const {
   getUser,
   updateUser,
   updateAvatar,
+  getCurrentUser,
 } = require('../controllers/user');
 
 router.get('/', getUsers);
+router.get('/me', getCurrentUser);
 router.get('/:userId', getUser);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateAvatar);
