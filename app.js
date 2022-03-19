6b4486bea7f464fee249d855d0efe0ef9d127111
Avellow <avellow@yandex.ru>
2022-03-19 10:28:50 +0300

    feature добавить новые роуты и обработчики

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/user');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6229d9b7565a0c46c52a40d3',
-  };
-
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
-
 app.post('/signin', login);
 app.post('/signup', createUser);
+
+app.use(auth);
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Ресурс не найден!' });
