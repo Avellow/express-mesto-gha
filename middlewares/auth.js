18318f0a4aacb7f443f51ba9bd5df22e15a3412b
Avellow <avellow@yandex.ru>
2022-03-22 16:30:23 +0300

    modify изменить обработку ошибки токена

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { handleAuthError } = require('../errors/errors');
+const TokenCheckError = require('../errors/TokenCheckError');
 
 const extractBearerHeader = (header) => header.replace('Bearer ', '');
 
@@ -15,8 +16,9 @@ module.exports = (req, res, next) => {
 
   try {
     payload = jwt.verify(token, 'some-secret');
-  } catch (err) {
-    return handleAuthError(res);
+  } catch (e) {
+    const err = new TokenCheckError('Некорректный токен');
+    next(err);
   }
 
   req.user = payload;
