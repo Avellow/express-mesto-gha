426791298bf823ca7968968c9189b24ac81cac66
Avellow <avellow@yandex.ru>
2022-03-23 14:03:43 +0300

    modify заменить стандартный вызов ошибки на ошибку при аутентификации

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const isEmail = require('validator/lib/isEmail');
+const AuthError = require('../errors/AuthError');
 
 function validateLink(v) {
   const regex = /http[s]?:\/\/(www.)?[\S]+\.[a-z]+[\S]*/gi;
@@ -51,14 +52,14 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(email,
     .select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль!'));
+        return Promise.reject(new AuthError('Неправильные почта или пароль!'));
       }
 
       return bcrypt
         .compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль!'));
+            return Promise.reject(new AuthError('Неправильные почта или пароль!'));
           }
 
           return user;
